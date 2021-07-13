7113675b4eeba3e8b8d7f1fa4003938bb52be9f5
Allen Zhong <allen@moe.cat>
2021-07-13 16:40:35 +0800

    add policy uri of signature

--- src/hockeypuck/openpgp/signature.go
+++ src/hockeypuck/openpgp/signature.go
@@ -34,6 +34,7 @@ type Signature struct {
 	RIssuerKeyID     string
 	Creation         time.Time
 	Expiration       time.Time
+	PolicyURI        string
 	Primary          bool
 	RevocationReason *packet.ReasonForRevocation
 }
@@ -126,6 +127,9 @@ func (sig *Signature) setSignature(s *packet.Signature, keyCreationTime time.Tim
 		sigKeyId := hex.EncodeToString(issuerKeyId[:])
 		sig.RIssuerKeyID = Reverse(sigKeyId)
 	}
+
+	// Extract the policy URI
+	sig.PolicyURI = s.PolicyURI
 
 	// Expiration time
 	if s.SigLifetimeSecs != nil {
